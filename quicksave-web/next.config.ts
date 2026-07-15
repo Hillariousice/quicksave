1db01beceff4c9297df0155dd3f888ea69cbb5d4
Hillary Okporka <hillariousice@gmail.com>
2026-07-15 21:47:31 +0100

    Add redirect from root to login page

--- quicksave-web/next.config.ts
+++ quicksave-web/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login',
+        permanent: true, // or false depending on your needs
+      },
+    ]
+  },
    images: {
     remotePatterns: [
       {
