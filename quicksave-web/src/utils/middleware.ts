3f8b8d3d92b54c1840125984e3ca7c6902c7750c
Hillary Okporka <hillariousice@gmail.com>
2026-07-15 21:46:27 +0100

    Update middleware config to include root path

--- quicksave-web/src/utils/middleware.ts
+++ quicksave-web/src/utils/middleware.ts
@@ -2,5 +2,5 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   // 👉 This locks down the /dashboard route and all its children!
-  matcher: ["/dashboard/:path*"] 
+  matcher: ["/","/dashboard/:path*"] 
 };
